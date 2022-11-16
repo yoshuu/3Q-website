b12ede5bca20add62fa9011781df3ae4160eea30
yoshuu <appcs342@gmail.com>
2022-11-17 07:05:40 +0800

    feat: add 整理Front放到APP.vue中

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,680 @@
-<script setup>
-// import { RouterLink, RouterView } from "vue-router";
-import Title from "./components/Title.vue";
-</script>
+<script setup></script>
 
 <template>
-  <h1>佳憲測試中</h1>
-  <Suspense>
-    <Title></Title>
-  </Suspense>
+  <header>
+    <div class="header_container">
+      <div class="slogan_container">
+        <div>
+          <p>會說話</p>
+          <p>會做事</p>
+        </div>
+        <P>台灣基進立法委員</P>
+        <h1>陳柏惟</h1>
+      </div>
+      <div class="people_info">
+        <img src="../../src/assets/images/people-01.png" alt="形象照" />
+      </div>
+      <div class="duration_container">
+        <p>
+          上任第 <span>100</span> 天 <span>14</span> 時 <span>2</span> 分
+          <span>30</span> 秒
+        </p>
+        <p>立院出席率<span>100%</span></p>
+      </div>
+      <div class="data_prove">
+        <img src="@/assets/images/title-decoration.png" alt="" />
+      </div>
+    </div>
+  </header>
+  <main>
+    <div class="card_container">
+      <div class="test1">
+        <div>
+          <div>
+            <p>12</p>
+            <h2>法律主提案</h2>
+          </div>
+          <div>
+            <p>12</p>
+            <h2>法律共同提案</h2>
+          </div>
+          <div>
+            <p>98</p>
+            <h2>書面質詢</h2>
+          </div>
+          <div>
+            <p>18</p>
+            <h2>口頭質詢</h2>
+          </div>
+          <div>
+            <p>98</p>
+            <h2>其他國會發言</h2>
+          </div>
+        </div>
+        <div class="details">
+          <p>詳細內容</p>
+          <svg
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class=""
+          >
+            <circle
+              cx="18"
+              cy="18"
+              r="17"
+              transform="rotate(-90 18 18)"
+              stroke="#FFF"
+              stroke-width="2"
+            ></circle>
+            <path
+              d="M9.5 18h16.25M21 23l5-5-5-5"
+              stroke="#FFF"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+          <!-- <i class="fa-solid fa-arrow-right"></i> -->
+        </div>
+      </div>
+      <div class="test2">
+        <div>
+          <p>2273</p>
+          <h2>陳情案件</h2>
+        </div>
+        <div class="details">
+          <p>詳細內容</p>
+          <svg
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class=""
+          >
+            <circle
+              cx="18"
+              cy="18"
+              r="17"
+              transform="rotate(-90 18 18)"
+              stroke="#FFF"
+              stroke-width="2"
+            ></circle>
+            <path
+              d="M9.5 18h16.25M21 23l5-5-5-5"
+              stroke="#FFF"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+          <!-- <i class="fa-solid fa-arrow-right"></i> -->
+        </div>
+      </div>
+      <div class="test3">
+        <div>
+          <p>104</p>
+          <h2>會勘及地方建設</h2>
+        </div>
+        <div class="details">
+          <p>詳細內容</p>
+          <svg
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class=""
+          >
+            <circle
+              cx="18"
+              cy="18"
+              r="17"
+              transform="rotate(-90 18 18)"
+              stroke="#FFF"
+              stroke-width="2"
+            ></circle>
+            <path
+              d="M9.5 18h16.25M21 23l5-5-5-5"
+              stroke="#FFF"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+          <!-- <i class="fa-solid fa-arrow-right"></i> -->
+        </div>
+      </div>
+      <div class="test4">
+        <div>
+          <p>13</p>
+          <h2>主辦記者會</h2>
+        </div>
+        <div class="details">
+          <p>詳細內容</p>
+          <svg
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class=""
+          >
+            <circle
+              cx="18"
+              cy="18"
+              r="17"
+              transform="rotate(-90 18 18)"
+              stroke="#FFF"
+              stroke-width="2"
+            ></circle>
+            <path
+              d="M9.5 18h16.25M21 23l5-5-5-5"
+              stroke="#FFF"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+          <!-- <i class="fa-solid fa-arrow-right"></i> -->
+        </div>
+      </div>
+      <div class="test5">
+        <div>
+          <p>79</p>
+          <h2>社會及公益活動</h2>
+        </div>
+        <div class="details">
+          <p>詳細內容</p>
+          <svg
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class=""
+          >
+            <circle
+              cx="18"
+              cy="18"
+              r="17"
+              transform="rotate(-90 18 18)"
+              stroke="#FFF"
+              stroke-width="2"
+            ></circle>
+            <path
+              d="M9.5 18h16.25M21 23l5-5-5-5"
+              stroke="#FFF"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+          <!-- <i class="fa-solid fa-arrow-right"></i> -->
+        </div>
+      </div>
+    </div>
+    <div class="main_slogan_container">
+      <h2><span>不只會做事</span><span>更重視鄉親</span></h2>
+      <div>
+        <div>
+          <p>4964</p>
+          <h3>地方行程</h3>
+        </div>
+        <div>
+          <p>1846</p>
+          <h3>鄉親拜訪</h3>
+        </div>
+      </div>
+    </div>
+  </main>
+  <footer>
+    <div class="footer_container">
+      <div class="article_container">
+        <span>
+          我不是完人，一定會有犯錯的時候，得承認反覆檢討並不好受，但我很清楚有多少人的期望在我身上，而我不能辜負這些託付，我不能說我特別累，事實上每一個想實踐理想的政治人物，都是這樣過生活。這是我對每一位幫助過我的、每一位投過我票的，能做出的最大感謝。 </span
+        ><span
+          >明天起床依然要在前線作戰，面對這個世界光怪陸離的事情，我會繼續勇敢奮戰。</span
+        ><br /><span>我是台灣基進的立法委員3Q陳柏惟。</span
+        ><span>大風大浪， 一起前行。</span>
+      </div>
+      <div class="community_list">
+        <div class="community_add">
+          <p>加入</p>
+          <a class="community_add_line" href="https://lin.ee/B4TeoDx">
+            <svg
+              width="36"
+              height="36"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M18 7.923c-6.064 0-11 4.045-11 9.016 0 4.453 3.914 8.183 9.199 8.893.358.076.846.239.97.546.11.279.072.71.034 1l-.15.943c-.041.28-.22 1.098.962.597 1.183-.498 6.34-3.774 8.65-6.455C28.244 20.715 29 18.924 29 16.939c0-4.97-4.936-9.016-11-9.016zm-4.306 11.972h-2.187a.584.584 0 01-.577-.582v-4.415c0-.32.261-.583.577-.583.32 0 .578.263.578.583v3.832h1.61a.58.58 0 01.576.583.582.582 0 01-.577.582zm2.26-.582a.582.582 0 01-.578.582.581.581 0 01-.574-.582v-4.415c0-.32.258-.583.577-.583.317 0 .576.263.576.583v4.415zm5.264 0a.582.582 0 01-.579.58.565.565 0 01-.467-.231l-2.24-3.07v2.72a.58.58 0 01-.578.583.58.58 0 01-.574-.583v-4.414a.58.58 0 01.572-.582c.179 0 .344.097.454.236l2.256 3.082v-2.736c0-.32.259-.583.578-.583.316 0 .578.263.578.583v4.415zm3.533-2.791c.32 0 .578.264.578.584 0 .32-.258.583-.578.583h-1.609v1.041h1.61a.58.58 0 01.577.583.581.581 0 01-.578.582h-2.187a.581.581 0 01-.575-.582v-4.415c0-.32.259-.583.578-.583h2.187c.317 0 .575.263.575.583a.58.58 0 01-.578.583h-1.609v1.04h1.61z"
+                fill="#A63F24"
+              ></path>
+            </svg>
+          </a>
+          <a
+            class="community_add_facebook"
+            href="https://www.facebook.com/3Q.PehUi"
+          >
+            <svg
+              width="36"
+              height="36"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                data-v-88897794=""
+                d="M24.574 9H11.426A2.426 2.426 0 009 11.425v13.15A2.425 2.425 0 0011.426 27h6.484l.012-6.432H16.25a.394.394 0 01-.394-.393l-.008-2.073c0-.219.176-.396.394-.396h1.668v-2.004c0-2.325 1.42-3.59 3.494-3.59h1.702c.218 0 .395.176.395.394v1.748a.394.394 0 01-.394.394h-1.045c-1.128 0-1.346.537-1.346 1.323v1.735h2.478c.236 0 .42.206.392.44l-.246 2.074a.394.394 0 01-.392.348h-2.221L20.716 27h3.858A2.425 2.425 0 0027 24.575v-13.15A2.426 2.426 0 0024.574 9z"
+                fill="#A63F24"
+              ></path>
+            </svg>
+          </a>
+        </div>
+        <div class="community_share">
+          <p>分享至</p>
+          <a href="##">
+            <svg
+              width="36"
+              height="36"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M18 7.923c-6.064 0-11 4.045-11 9.016 0 4.453 3.914 8.183 9.199 8.893.358.076.846.239.97.546.11.279.072.71.034 1l-.15.943c-.041.28-.22 1.098.962.597 1.183-.498 6.34-3.774 8.65-6.455C28.244 20.715 29 18.924 29 16.939c0-4.97-4.936-9.016-11-9.016zm-4.306 11.972h-2.187a.584.584 0 01-.577-.582v-4.415c0-.32.261-.583.577-.583.32 0 .578.263.578.583v3.832h1.61a.58.58 0 01.576.583.582.582 0 01-.577.582zm2.26-.582a.582.582 0 01-.578.582.581.581 0 01-.574-.582v-4.415c0-.32.258-.583.577-.583.317 0 .576.263.576.583v4.415zm5.264 0a.582.582 0 01-.579.58.565.565 0 01-.467-.231l-2.24-3.07v2.72a.58.58 0 01-.578.583.58.58 0 01-.574-.583v-4.414a.58.58 0 01.572-.582c.179 0 .344.097.454.236l2.256 3.082v-2.736c0-.32.259-.583.578-.583.316 0 .578.263.578.583v4.415zm3.533-2.791c.32 0 .578.264.578.584 0 .32-.258.583-.578.583h-1.609v1.041h1.61a.58.58 0 01.577.583.581.581 0 01-.578.582h-2.187a.581.581 0 01-.575-.582v-4.415c0-.32.259-.583.578-.583h2.187c.317 0 .575.263.575.583a.58.58 0 01-.578.583h-1.609v1.04h1.61z"
+                fill="#A63F24"
+              ></path>
+            </svg>
+          </a>
+          <a href="##">
+            <svg
+              width="36"
+              height="36"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class=""
+            >
+              <path
+                d="M24.574 9H11.426A2.426 2.426 0 009 11.425v13.15A2.425 2.425 0 0011.426 27h6.484l.012-6.432H16.25a.394.394 0 01-.394-.393l-.008-2.073c0-.219.176-.396.394-.396h1.668v-2.004c0-2.325 1.42-3.59 3.494-3.59h1.702c.218 0 .395.176.395.394v1.748a.394.394 0 01-.394.394h-1.045c-1.128 0-1.346.537-1.346 1.323v1.735h2.478c.236 0 .42.206.392.44l-.246 2.074a.394.394 0 01-.392.348h-2.221L20.716 27h3.858A2.425 2.425 0 0027 24.575v-13.15A2.426 2.426 0 0024.574 9z"
+                fill="#A63F24"
+              ></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div class="footer_people">
+        <img src="@/assets/images/people-02.png" alt="形象照二" />
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped lang="scss">
-h1 {
-  color: $primary;
+header {
+  .header_container {
+    position: relative;
+    min-width: 375px;
+    background: url(../../src/assets/images/logo.png) no-repeat -130px -97px;
+    @include breakpoint($md) {
+      background: url(../../src/assets/images/logo.png) no-repeat -104px -155px;
+      background-size: 488px 488px;
+    }
+  }
+  .slogan_container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 51px;
+    @include breakpoint($md) {
+      align-items: flex-start;
+      padding-bottom: 40px;
+      padding-left: 88px;
+    }
+    @include breakpoint($lg) {
+      padding: 72px 140px 40px;
+    }
+    > div {
+      @include breakpoint($md) {
+        gap: 30px;
+      }
+    }
+  }
+  .slogan_container,
+  h1 {
+    color: $primary;
+    font-weight: bold;
+    font-size: 65px;
+  }
+  .slogan_container > p:nth-child(2) {
+    font-size: 24px;
+  }
+
+  .people_info {
+    margin: -22px auto 0;
+    min-width: 360px;
+    width: 50vw;
+    @include breakpoint($md) {
+      position: absolute;
+      z-index: 1;
+      max-width: 681.74px;
+      top: 280px;
+      right: 50px;
+    }
+    @include breakpoint($xxl) {
+      top: 83.74px;
+      right: 50.27px;
+    }
+    img {
+      width: 100%;
+      transform: rotate(4.81deg);
+      object-fit: contain;
+    }
+  }
+
+  .duration_container {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    bottom: 145px;
+    width: 100%;
+    gap: 8px;
+
+    @include breakpoint($md) {
+      flex-direction: row;
+      width: unset;
+      gap: 16px;
+      top: 25px;
+      right: 25px;
+      bottom: unset;
+    }
+    p {
+      padding: 7px 21px;
+      color: white;
+      font-weight: 900;
+      font-size: 15px;
+      background: $primary;
+    }
+  }
+
+  .data_prove {
+    display: none;
+    @include breakpoint($lg) {
+      display: block;
+      position: absolute;
+      top: 204px;
+      left: 408px;
+    }
+  }
+}
+main {
+  .card_container {
+    margin-top: -129px;
+    color: $primary;
+    font-weight: 900;
+    @include breakpoint($md) {
+    }
+    @include breakpoint($md) {
+      margin-top: unset;
+      padding: 0 88px;
+      display: grid;
+      grid-template-rows: 40vw 2vw 18vw 2vw 2vw 20vw;
+      grid-template-columns: 32.1% 1.72% 18.5% 13.5% 1.72% 32.1%;
+    }
+    @include breakpoint($lg) {
+      padding: 0 140px;
+    }
+    @include breakpoint($xxl) {
+      display: grid;
+      grid-template-rows: 436px 20px 210px 20px 20px 230px;
+      grid-template-columns: 373px 20px 215.6px 157.4px 20px 373px;
+    }
+
+    > div {
+      position: relative;
+      margin: 0 16px 16px;
+      padding: 16px 16px;
+      min-height: 205px;
+
+      &:hover {
+        svg {
+          transform: rotate(90deg);
+          transition-duration: 2s;
+          transition-property: transform;
+          circle,
+          path {
+            stroke: white;
+          }
+        }
+        color: white;
+        background: $primary;
+      }
+      @include breakpoint($md) {
+        margin: 0;
+        min-height: unset;
+      }
+      @include breakpoint($xl) {
+        padding: 24px 30px;
+      }
+    }
+    h2 {
+      font-size: 17px;
+      @include breakpoint($xl) {
+        font-size: 24px;
+      }
+    }
+    p {
+      font-size: 50px;
+      line-height: 50px;
+      @include breakpoint($xl) {
+        font-size: 80px;
+        line-height: 80px;
+      }
+    }
+    .details {
+      position: absolute;
+      display: flex;
+      align-items: center;
+      bottom: 16px;
+      right: 16px;
+      @include breakpoint($xxl) {
+        bottom: 30px;
+        right: 30px;
+      }
+      p {
+        font-size: 15px;
+        @include breakpoint($xxl) {
+          font-size: 24px;
+        }
+      }
+
+      svg {
+        margin-left: 16px;
+        circle,
+        path {
+          stroke: $primary;
+        }
+        @include breakpoint($lg) {
+          width: 54px;
+          height: 54px;
+        }
+      }
+    }
+  }
+  .test1 {
+    grid-area: 1/1/1/4;
+    background: url(@/assets/images/proposal-bg.png) no-repeat 190px bottom
+      #f6f6f6;
+    background-size: 298px 152px;
+    @include breakpoint($lg) {
+      background-size: 510px 260px;
+      background-position: 290px 176px;
+    }
+
+    > div:nth-child(1) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      width: 256px;
+      @include breakpoint($xl) {
+        width: 355px;
+      }
+      > div {
+        flex-basis: 104px;
+        @include breakpoint($xl) {
+          flex-basis: 150px;
+        }
+      }
+      > div:nth-child(n + 3) {
+        margin-top: 17px;
+        @include breakpoint($lg) {
+          margin-top: 24px;
+        }
+      }
+    }
+  }
+  .test2 {
+    grid-area: 3/1/5/2;
+    background: url(@/assets/images/petition-bg.png) no-repeat 169px 75px
+      #f6f6f6;
+    background-size: 154px 169px;
+    @include breakpoint($lg) {
+      background-size: 175px 208.56px;
+      background-position: 175px 92.8px;
+    }
+  }
+  .test3 {
+    grid-area: 3/3/7/5;
+    background: url(@/assets/images/survey-bg-lg.png) no-repeat 171px 16px
+      #f6f6f6;
+    background-size: 151px 279px;
+    @include breakpoint($lg) {
+      background-size: 254px 468.38px;
+      background-position: 124px 56px;
+    }
+
+    > div {
+      @include breakpoint($md) {
+        text-align: center;
+      }
+    }
+    .details {
+      @include breakpoint($md) {
+        justify-content: center;
+        position: unset;
+      }
+    }
+  }
+  .test4 {
+    grid-area: 6/1/6/2;
+    background: url(@/assets/images/press-conference-bg.png) no-repeat -26px 18px
+      #f6f6f6;
+    background-size: 507px 308px;
+    @include breakpoint($lg) {
+      background-size: 672px 395.95px;
+      background-position: -148px 32.03px;
+    }
+  }
+  .test5 {
+    grid-area: 4/6/7/7;
+    background: url(@/assets/images/welfare-bg.png) no-repeat 87px 25px #f6f6f6;
+    background-size: 271.55px 228px;
+    @include breakpoint($lg) {
+      background-size: 318px 267px;
+      background-position: 113px 25px;
+    }
+    @include breakpoint($md) {
+      z-index: 2;
+    }
+  }
+  .main_slogan_container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 16px;
+    align-items: center;
+    padding: 30px 0;
+    text-align: center;
+    font-weight: 900;
+    color: white;
+    background-color: $primary;
+    @include breakpoint($md) {
+      padding: 35px 0 49px;
+      gap: 27px;
+    }
+    h2 {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      font-size: 40px;
+      @include breakpoint($md) {
+        flex-direction: row;
+        margin-bottom: 27px;
+        font-size: 60px;
+        gap: 20px;
+      }
+    }
+    h3 {
+      font-size: 17px;
+      @include breakpoint($md) {
+        font-size: 24px;
+      }
+    }
+    p {
+      font-size: 50px;
+      @include breakpoint($md) {
+        font-size: 100px;
+      }
+    }
+    > div {
+      @include breakpoint($md) {
+        display: flex;
+        justify-content: center;
+        gap: 99px;
+      }
+    }
+  }
+}
+footer {
+  .footer_container {
+    position: relative;
+    padding: 30px 16px 45px;
+    font-size: 17px;
+    @include breakpoint($xxl) {
+      padding: 48px 50px 50px 556px;
+    }
+  }
+  .article_container {
+    display: flex;
+    flex-direction: column;
+  }
+  .community_list {
+    display: flex;
+    gap: 36px;
+  }
+  .community_add,
+  .community_share {
+    align-items: center;
+    display: flex;
+    gap: 4px;
+  }
+
+  .footer_people {
+    display: none;
+    width: 442px;
+    @include breakpoint($xxl) {
+      display: block;
+      position: absolute;
+      left: 32px;
+      bottom: -78px;
+    }
+
+    img {
+      width: 100%;
+    }
+  }
 }
 </style>
