33f501d63fdaa64718705b9e3125404c07f6d249
yoshuu <appcs342@gmail.com>
2022-11-16 16:38:01 +0800

    feat: add 新增api的程式

---
+++ src/components/Test.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { GET } from "@/api/api.js";
+
+const apple = await GET("?maxRecords=100&view=Grid%20view");
+console.log(apple);
+const book = apple.data.records;
+</script>
+<template>
+  <div v-for="item in book" :key="item.id">
+    <h1>我的時間{{ item.createdTime }}</h1>
+    <h2>再來一次{{ item.id }}</h2>
+  </div>
+</template>
+<style scoped></style>
