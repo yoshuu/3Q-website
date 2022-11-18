915d2d268f8d8f906b2512984e43bd19b386e72c
William lai <william>
2022-11-18 12:46:10 +0800

    backup

---
+++ src/components/Calendar.vue
@@ -0,0 +1,36 @@
+<script>
+import "@fullcalendar/core/vdom"; // solves problem with Vite
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+export default {
+  components: {
+    FullCalendar, // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: "dayGridMonth",
+        dateClick: this.handleDateClick,
+        events: [
+          { title: "event 1", date: "2019-04-01" },
+          { title: "event 2", date: "2019-04-02" },
+        ],
+      },
+    };
+  },
+  methods: {
+    handleDateClick: function (arg) {
+      alert("date click! " + arg.dateStr);
+    },
+  },
+};
+</script>
+
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
+
+<style scoped lang="scss"></style>
