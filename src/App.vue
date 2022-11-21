533bd7dcfad634acbd5e7fb1a51630880968d6b3
yoshuu <appcs342@gmail.com>
2022-11-22 04:46:45 +0800

    fix: modify更改app.vue變成原本樣子

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script setup>
-import Calendar from "./components/calendar.vue";
+import Front_header from "./components/Front_header.vue";
+import Front_main from "./components/Front_main.vue";
+import Front_footer from "./components/Front_footer.vue";
 </script>
-
 <template>
-  <Calendar />
+  <Suspense>
+    <div class="front">
+      <header>
+        <Front_header />
+      </header>
+      <main>
+        <Front_main />
+      </main>
+      <footer>
+        <Front_footer />
+      </footer>
+    </div>
+  </Suspense>
 </template>
 
 <style scoped lang="scss">
